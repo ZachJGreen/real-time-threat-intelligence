cc429c8ee3f26e0b995ac09ca4c016c980c10e33
halleepham <hdph78@umsystem.edu>
2025-03-05 17:17:33 -0600

    connected to the databse

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+const client = require("./pg")
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+
+//app.get('/', (req, res) => {
+    //res.send('API is running...')
+//});
+
+app.get('/',(req, res)=>{
+    res.send('hi database is connected!!!!')
+})
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
