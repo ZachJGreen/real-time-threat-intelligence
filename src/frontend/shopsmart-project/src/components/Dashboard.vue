c67b2cff622d89c9abba82417e7b63aa0f29316c
ZachJGreen <green.zach.j@gmail.com>
2025-03-04 20:31:25 -0600

    edited Dashboard.vue

--- src/frontend/shopsmart-project/src/components/Dashboard.vue
+++ src/frontend/shopsmart-project/src/components/Dashboard.vue
@@ -5,13 +5,16 @@
 
   <div class="box">
     <h2>Threat Logs</h2>
+    <p>foo</p>
   </div>
 
   <div class="box">
     <h2>Risk Scores</h2>
+    <p>bar</p>
   </div>
 
   <div class="box">
     <h2>Real-time Alerts</h2>
+    <p>fizz</p>
   </div>
 </template>
