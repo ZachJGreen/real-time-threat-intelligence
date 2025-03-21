d70075e8ef1339d24022f8edb0dde03caae06c95
halleepham <hdph78@umsystem.edu>
2025-03-20 23:23:26 -0500

    Updated asset list

--- db/tva_mapping.sql
+++ db/tva_mapping.sql
@@ -15,12 +15,25 @@ CREATE TABLE tva_mapping (
 -- add assets
 INSERT INTO assets (asset_name, asset_type, description) VALUES
     ('Server', 'Hardware', 'Hosts website and customer data'),
+    ('Workstations', 'Hardware', 'Used by employees to complete work'),
     ('Network Router', 'Hardware', 'Connects employee workstations to the internet'),
+    ('Firewall', 'Hardware', 'Serves as a security barrier between the company network and the internet'),
+    ('Switch', 'Hardware', 'Connects workstations and database server'),
     ('Inventory Management System', 'Software', 'Keeps track of ShopSmart inventory'),
-    ('MySQL', 'Software', 'Software', 'Stores all application data'),
+    ('CMS', 'Software', 'Allows employees to create, manage, and publish content to the web'),
+    ('Anti-Virus Software', 'Software', 'Protects devices from malware'),
+    ('MySQL', 'Software', 'Stores all application data'),
     ('Customer Information', 'Data', 'Includes name, address, and payment details'),
+    ('Product Inventory', 'Data', 'Available products and their relevant information'),
+    ('Sales Records', 'Data', 'Tracks transaction history of customer'),
+    ('Employee Information', 'Data', 'Personal information about employees of ShopSmart'),
+    ('Business Owner', 'People', 'Oversees operations and decision-making of ShopSmart'),
+    ('Customer Support Representatives', 'People', ' Interact with customers to answer questions'),
     ('IT Manager', 'People', 'Maintains IT infrastructure and updates inventory'),
-    ('Payments through payment gateway', 'Process', 'Handles customer payments through a third-party payment gateway');
+    ('Salesperson', 'People', 'Sells products to customers'),
+    ('Customer orders through website', 'Process', 'Orders are submitted and processed online'),
+    ('Payments through payment gateway', 'Process', 'Handles customer payments through a third-party payment gateway'),
+    ('Updating inventory', 'Process', 'IT manager ensures inventory records are accurate');
 
 -- add threats
 INSERT INTO threats (asset_id, threat_name) VALUES
