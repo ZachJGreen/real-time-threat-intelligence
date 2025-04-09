77dcb24a1041b78ca95e4a31bb9dec925dce1054
ZachJGreen <green.zach.j@gmail.com>
2025-04-08 22:58:14 -0500

    created scheduled data fetcher

--- src/backend/utils.js
+++ src/backend/utils.js
@@ -1,9 +1,16 @@
+const { fetchAndStoreShodanData } = require("../../api/fetch_osint");
+const schedule = require("node-schedule");
+
 // Risk Score Calculator
 function calculate_risk(likelihood, impact) { 
     return likelihood * impact;
 }
 
 // Data Fetch Scheduler
-function fetch_scheduler(){
-    
+function fetch_scheduler(ip){
+    // Execute fetchAndStoreShodanData function every 6 hours
+    // '* * 6' is 6 hours in cron notation
+    const job = schedule.scheduleJob('* * 6', (ip) => {
+        fetchAndStoreShodanData(ip)
+    });
 }
